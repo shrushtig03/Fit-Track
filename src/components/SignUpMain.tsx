7502c8287d46565f5eba6cf2044623970cf87fd1
tsahil01 <sahiltiwaskar.21@stvincentngp.edu.in>
2024-11-16 22:28:53 +0530

    signupMain

--- src/components/SignUpMain.tsx
+++ src/components/SignUpMain.tsx
@@ -1,12 +1,41 @@
+/* eslint-disable @next/next/no-img-element */
+"use client"
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { HeartPulse } from "lucide-react";
+import { signIn } from "next-auth/react";
 import Link from "next/link";
+import { useState } from "react";
 
 export function SignUpMain() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [username, setUsername] = useState("");
+    const [age, setAge] = useState(0);
+    const [gender, setGender] = useState("");
+
+    const handleSubmit = async () => {
+        console.log(email, password, username, age, gender);
+
+        const result = await signIn("credentials", {
+            email,
+            password,
+            username,
+            age,
+            gender,
+            redirect: true,
+            callbackUrl: "/",
+        });
+
+        if (!result?.ok) {
+            console.error("Error signing up:", result?.error);
+        }
+    };
+
+
     return (
         <div className="flex md:flex-row flex-col h-screen mx-auto min-w-screen">
             {/* Left Side - Image */}
@@ -36,25 +65,35 @@ export function SignUpMain() {
                             <div className="grid w-full gap-4">
                                 <div className="flex flex-col space-y-1.5">
                                     <Label htmlFor="email">Email</Label>
-                                    <Input id="email" type="email" placeholder="Enter your email" />
+                                    <Input id="email" type="email" placeholder="Enter your email" onChange={
+                                        (e) => setEmail(e.target.value)
+                                    } />
                                 </div>
                                 <div className="flex flex-col space-y-1.5">
                                     <Label htmlFor="password">Password</Label>
-                                    <Input id="password" type="password" placeholder="Enter your password" />
+                                    <Input id="password" type="password" placeholder="Enter your password" onChange={
+                                        (e) => setPassword(e.target.value)
+                                    } />
                                 </div>
                                 <div className="flex md:flex-row flex-col gap-4">
                                     <div className="flex flex-col space-y-1.5 w-full">
                                         <Label htmlFor="username">Username</Label>
-                                        <Input id="username" type="text" placeholder="Enter your username" />
+                                        <Input id="username" type="text" placeholder="Enter your username" onChange={
+                                            (e) => setUsername(e.target.value)
+                                        } />
                                     </div>
                                     <div className="flex flex-col space-y-1.5 w-full">
                                         <Label htmlFor="age">Age</Label>
-                                        <Input id="age" type="number" placeholder="Enter your age" />
+                                        <Input id="age" type="number" placeholder="Enter your age" onChange={
+                                            (e) => setAge(parseInt(e.target.value))
+                                        } />
                                     </div>
                                 </div>
 
                                 <div className="flex flex-row space-y-1.5 w-full justify-center">
-                                    <Select>
+                                    <Select onValueChange={
+                                        (value) => setGender(value)
+                                    }>
                                         <SelectTrigger className="w-full md:w-[180px]">
                                             <SelectValue placeholder="Select Gender" />
                                         </SelectTrigger>
@@ -73,7 +112,7 @@ export function SignUpMain() {
                         </form>
                     </CardContent>
                     <CardFooter className="flex justify-center flex-col">
-                        <Button>Sign Up</Button>
+                        <Button onClick={handleSubmit}>Sign Up</Button>
                         <p className="mx-auto text-base mt-2 text-primary/50 font-medium text-center">
                             {"Don't have an account? "}
                             <Link className="font-bold underline" href={"/signup"}>Sign In</Link>
